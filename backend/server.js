2e56b2dc9c667c6abec7ad22bfb6e700299a30f7
Swardcrafter <84284643+Swardcrafter@users.noreply.github.com>
2023-07-26 23:26:33 +0000

    1

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,34 @@
-const ws = new WebSocket("wss://Obsidian-Syncify-Main.saturnwillow.repl.co/echo");
+var express = require('express')
+var expressWs = require('express-ws')
 
-ws.addEventListener("open", () => {
-    console.log("We are connected");      
+var app = express()
+expressWs(app)
+
+
+app.ws('/echo', (ws) => {
+    ws.on("message", data => {
+        data = JSON.parse(data);
+	});
 });
 
-ws.addEventListener("message", msg => {
-    msg = JSON.parse(msg);
+const path=require('path');
+let oneStepBack=path.join(__dirname,'../');
+app.use("/backend", express.static(__dirname + '/backend'));
+app.get("/", (req, res) => {
+  // Send the index.html file
+  res.sendFile(path.join(oneStepBack, "frontend/index.html"));
+});
+
+app.get("/styles.css", (req, res) => {
+  // Send the styles.css file
+  res.sendFile(path.join(oneStepBack, "frontend/styles.css"));
+});
+
+app.get("/index.js", (req, res) => {
+  // Send the index.js file
+  res.sendFile(path.join(oneStepBack, "frontend/frontendws.js"));
+});
+app.listen(8081, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", 8081);
 });
