e4944e1132addea9bc97a25a31aeb990979fffd3
Swardcrafter <84284643+Swardcrafter@users.noreply.github.com>
2023-07-26 20:42:54 -0700

    1

--- frontend/frontendws.js
+++ frontend/frontendws.js
@@ -11,3 +11,27 @@ ws.addEventListener("open", () => {
 ws.addEventListener("message", msg => {
     msg = JSON.parse(msg);
 });
+
+function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Get the form data using FormData API
+    const form = event.target;
+    const formData = new FormData(form);
+
+    // Convert the FormData object to a regular object
+    const formDataObj = {};
+    formData.forEach((value, key) => {
+      formDataObj[key] = value;
+    });
+
+    // Convert the object to JSON
+    const jsonData = JSON.stringify(formDataObj);
+
+    console.log(jsonData);
+    
+ }
+
+// Add the event listener to the form submit event
+const loginForm = document.getElementById('login');
+loginForm.addEventListener('submit', handleFormSubmit);
