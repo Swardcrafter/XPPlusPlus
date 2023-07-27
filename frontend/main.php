02c1c0863d4b4537bf356a710b0ac06218f75478
Swardcrafter <84284643+Swardcrafter@users.noreply.github.com>
2023-07-26 20:34:13 -0700

    1

---
+++ frontend/main.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Email: <?php echo $_POST["email"]; ?><br>
+Password: <?php echo $_POST["password"]; ?><br>
+Confirm Password: <?php echo $_POST["confirmpassword"]; ?>
+
+</body>
+</html>
