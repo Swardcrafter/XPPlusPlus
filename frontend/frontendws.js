3391d357c33eba47f161deecda6b3036fa568d96
Swardcrafter <84284643+Swardcrafter@users.noreply.github.com>
2023-07-26 21:13:03 -0700

    1

--- frontend/frontendws.js
+++ frontend/frontendws.js
@@ -13,7 +13,7 @@ ws.addEventListener("message", msg => {
 });
 
 function handleFormSubmit(event) {
-	e.preventDefault();
+	event.preventDefault();
     // Get the form data using FormData API
     const form = event.target;
     const formData = new FormData(form);
