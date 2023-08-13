130d228ca947e0fc17ca3a3474e581c1c4d81f2b
Swardcrafter <84284643+Swardcrafter@users.noreply.github.com>
2023-08-13 07:09:45 +0000

    1

---
+++ frontend/index.js
@@ -0,0 +1,28 @@
+function copyToClipboard() {
+    const text = document.getElementById('output-text').innerText;
+    const textArea = document.createElement('textarea');
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+    alert('Copied to clipboard: ' + text);
+}
+
+function sendTheMessage() {
+	const text = document.getElementById('message').innerText;
+	const endvar = document.getElementById('end-variable').value;
+  const startvar = document.getElementById('start-variable').value;
+  const varnumb = document.getElementById('variable-numbers').value;
+  const varname = document.getElementById('variable-name').value;
+
+  send({
+		type: "message",
+		data: {
+			startvar: startvar,
+			endvar: endvar,
+			varnumb: varnumb,
+			varname: varname
+		}
+	})
+}
